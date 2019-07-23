9b0bad9268a48917ea6a8b06872bc3bd99c97e94
muhammadnazmih <muhammadnazmi442@gmail.com>
2019-07-23 23:39:00 +0700

    versi 2.3.2

---
+++ MyRecycleView/app/src/main/java/com/example/nazmiapps/myrecycleview/ListHeroAdapter.java
@@ -0,0 +1,60 @@
+package com.example.nazmiapps.myrecycleview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> {
+
+    private ArrayList<Hero> listHero;
+
+    public ListHeroAdapter(ArrayList<Hero> list) {
+        this.listHero = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_row_hero, viewGroup, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ListViewHolder listViewHolder, int position) {
+        Hero hero = listHero.get(position);
+        Glide.with(listViewHolder.itemView.getContext())
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .into(listViewHolder.imgPhoto);
+
+        listViewHolder.tvName.setText(hero.getName());
+        listViewHolder.tvFrom.setText(hero.getFrom());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listHero.size();
+    }
+
+    class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName, tvFrom;
+
+         ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+             imgPhoto = itemView.findViewById(R.id.img_item_photo);
+             tvName = itemView.findViewById(R.id.tv_item_name);
+             tvFrom = itemView.findViewById(R.id.tv_item_from);
+        }
+    }
+}
